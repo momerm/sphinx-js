7b3b4ce23895cd7a802d6c934faab1af440b748a
Omer Mirza <zcabmom@ucl.ac.uk>
2018-02-15 10:47:36 +0000

    Added benchmarks

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,42 @@ module.exports = function(grunt) {
                 }
             }
         },
+        browserify: {
+            benchmark: {
+                src: ["benchmark/browser/Benchmark.js"],
+                dest: "benchmark/browser/Benchmark.bundle.js",
+                options: {
+                    alias: [
+                        './node_modules/milagro-crypto-js/src/aes.js:./aes',
+                        './node_modules/milagro-crypto-js/src/big.js:./big',
+                        './node_modules/milagro-crypto-js/src/ecdh.js:./ecdh',
+                        './node_modules/milagro-crypto-js/src/ecp.js:./ecp',
+                        './node_modules/milagro-crypto-js/src/ecp2.js:./ecp2',
+                        './node_modules/milagro-crypto-js/src/ff.js:./ff',
+                        './node_modules/milagro-crypto-js/src/fp.js:./fp',
+                        './node_modules/milagro-crypto-js/src/fp2.js:./fp2',
+                        './node_modules/milagro-crypto-js/src/fp4.js:./fp4',
+                        './node_modules/milagro-crypto-js/src/fp12.js:./fp12',
+                        './node_modules/milagro-crypto-js/src/gcm.js:./gcm',
+                        './node_modules/milagro-crypto-js/src/hash256.js:./hash256',
+                        './node_modules/milagro-crypto-js/src/hash384.js:./hash384',
+                        './node_modules/milagro-crypto-js/src/hash512.js:./hash512',
+                        './node_modules/milagro-crypto-js/src/mpin.js:./mpin',
+                        './node_modules/milagro-crypto-js/src/newhope.js:./newhope',
+                        './node_modules/milagro-crypto-js/src/nhs.js:./nhs',
+                        './node_modules/milagro-crypto-js/src/pair.js:./pair',
+                        './node_modules/milagro-crypto-js/src/rand.js:./rand',
+                        './node_modules/milagro-crypto-js/src/rom_curve.js:./rom_curve',
+                        './node_modules/milagro-crypto-js/src/rom_field.js:./rom_field',
+                        './node_modules/milagro-crypto-js/src/rsa.js:./rsa',
+                        './node_modules/milagro-crypto-js/src/sha3.js:./sha3',
+                        './node_modules/milagro-crypto-js/src/uint64.js:./uint64'
+                    ]
+                }
+            }
+        }
     });
 
     grunt.loadNpmTasks('grunt-patcher');
+    grunt.loadNpmTasks("grunt-browserify");
 };
---
+++ benchmark/node/Benchmark.js
@@ -0,0 +1,80 @@
+/*
+Copyright 2011 Ian Goldberg
+Copyright 2016 George Danezis (UCL InfoSec Group)
+Copyright 2018 Omer Mirza
+
+This file is part of Sphinx.
+
+Sphinx is free software: you can redistribute it and/or modify
+it under the terms of version 3 of the GNU Lesser General Public
+License as published by the Free Software Foundation.
+
+Sphinx is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with Sphinx.  If not, see
+<http://www.gnu.org/licenses/>.
+
+The LIONESS implementation and the xcounter CTR mode class are adapted
+from "Experimental implementation of the sphinx cryptographic mix
+packet format by George Danezis".
+*/
+
+const SphinxParams = require("../../lib/SphinxParams");
+const SC = require("../../lib/SphinxClient");
+const sphinx_process = require("../../lib/SphinxNode").sphinx_process;
+
+const bytesjs = require("bytes.js");
+const r = 5;
+const params = new SphinxParams();
+
+
+let nodes_routing = [];
+let node_priv_keys = [];
+let node_pub_keys = [];
+
+for(let i = 0; i < r; i++) {
+    let nid = "node" + i;
+    let x = params.group.gensecret();
+    let y = params.group.expon(params.group.g, x);
+
+    nodes_routing.push(SC.nenc(nid));
+    node_priv_keys.push(x);
+    node_pub_keys.push(y);
+}
+
+let dest = bytesjs.fromString("bob");
+let message = bytesjs.fromString("this is a test");
+let t0, t1;
+
+console.log("Testing message encoding time");
+let header, delta;
+let bin_message;
+t0 = Date.now();
+
+for(let i = 0; i < 100; i++) {
+    [header, delta] = SC.create_forward_message(params, nodes_routing, node_pub_keys, dest, message);
+    bin_message = SC.pack_message(params, [header, delta]);
+}
+
+t1 = Date.now();
+console.log("Encoding took " + ((t1 - t0) / 100) + " milliseconds.");
+
+
+console.log("Testing message processing time");
+let x = node_priv_keys[0];
+let lens = JSON.stringify([params.max_len, params.m]);
+let param_dict = {};
+param_dict[lens] = params;
+t0 = Date.now();
+
+for(let i = 0; i < 100; i++) {
+    [header, delta] = SC.unpack_message(param_dict, params.ctx, bin_message)[1];
+    sphinx_process(params, x, header, delta);
+}
+
+t1 = Date.now();
+console.log("processing took " + ((t1 - t0) / 100) + " milliseconds.");
